1d114fc7fad379db48c0f7f1d4336636e47b10fd
iksunchoi <iksunchoi0525@gmail.com>
2024-02-23 16:48:37 +0900

    basic setting project

---
+++ src/main/java/com/sun/zzim/ZzimApplication.java
@@ -0,0 +1,13 @@
+package com.sun.zzim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/zzim/ZzimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.zzim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
