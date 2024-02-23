e8f8686537726e0c090a9dbacc63f54012724de7
iksun <iksunchoi0525@gmail.com>
2024-02-23 20:20:07 +0900

    [user] create SecurityConfig for simple set api POST users

---
+++ src/main/java/com/sun/zzim/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sun.zzim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/users").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/users"))
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+}
