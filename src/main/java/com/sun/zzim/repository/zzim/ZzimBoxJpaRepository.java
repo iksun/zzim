bf7ef3c581f8f4479bd8ecd7d63cd845e38bdc76
iksun <iksunchoi0525@gmail.com>
2024-02-24 12:19:30 +0900

    [zzim-box] Create zzim box datamodel and repository

---
+++ src/main/java/com/sun/zzim/repository/zzim/ZzimBoxJpaRepository.java
@@ -0,0 +1,12 @@
+package com.sun.zzim.repository.zzim;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ZzimBoxJpaRepository extends JpaRepository<ZzimBoxDataModel, Long> {
+    List<ZzimBoxDataModel> findAllByUserId(long userId, Pageable pageable);
+}
